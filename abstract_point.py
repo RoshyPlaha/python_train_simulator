d0f96be3ba9f13ce2b7d2b80f4eee80591b190b2
Roshy <roshy@Roshys-MacBook-Air.local>
2020-05-06 19:00:36 +0100

    added abstract class so hold point value

---
+++ abstract_point.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+class abstract_point(ABC):
+
+    point = None
+
+    @abstractmethod
+    def next_point():
+        self.point
--- point.py
+++ point.py
@@ -1,12 +1,15 @@
-class point(object):
+import abstract_point
+class point(abstract_point.abstract_point):
 
     route_paths = []
 
     def __init__(self, point=None):
         self.point = point
-        self.position = 0
+        self.active_position = 0
         self.route_paths = []
 
+    def next_point(self):
+        print('ye')
 
     def add_paths(self, p):
         if len(self.route_paths) <= 2: # max paths for a point
@@ -17,30 +20,33 @@ class point(object):
 
     def get_active_path(self):
         for i, p in enumerate(self.route_paths):
-            if i == self.position:
+            if i == self.active_position:
                 return p
 
     def switch_path(self):
-        self.position +=1
-        if self.position > len(self.route_paths)-1:
-            self.position = 0
+        self.active_position +=1
+        if self.active_position > len(self.route_paths)-1:
+            self.active_position = 0
         for i, p in enumerate(self.route_paths):
-            if i == self.position:
+            if i == self.active_position:
                 return p
     
     def draw(self, pygame, screen):
         for path in self.route_paths:
             for route in path:
                 route.draw()
-
-        # active path with routes inside
-        active_start_xy = self.route_paths[self.position][0].get_start_route_coord()
-        active_end_xy = self.route_paths[self.position][0].get_end_route_coord()
-
+        
+        self.draw_active_position(pygame, screen)
+        self.draw_point_controller(pygame, screen)
+        
+    def draw_active_position(self, pygame, screen):
+        active_start_xy = self.route_paths[self.active_position][0].get_start_route_coord()
+        active_end_xy = self.route_paths[self.active_position][0].get_end_route_coord()
         y_start = active_start_xy[1]-2
         y_end = active_end_xy[1]-2
-
-        pygame.draw.aalines(screen, (0, 102, 204), False, [(active_start_xy[0], y_start), (active_end_xy[0], y_end)])
+        pygame.draw.lines(screen, (0, 102, 204), False, [(active_start_xy[0], y_start), (active_end_xy[0], y_end)], 3)
+    
+    def draw_point_controller(self, pygame, screen):
 
         # draw triangle below point
         triangle_top_point_coord_x = self.route_paths[0][0].get_start_route_coord()[0]
